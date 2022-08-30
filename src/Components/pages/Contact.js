c19479d6281c73922da519d9a184ade220568e91
sakau <sakau@usc.edu>
2022-08-29 18:48:23 -0700

    added header, navbar, projects tab

---
+++ src/Components/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../Header'
+
+const Contact = () => {
+    return (
+        <div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><i class="fa-brands fa-github"></i> Github</li>
+                <li class="list-group-item"><i class="fa-solid fa-at"></i> Email</li>
+                <li class="list-group-item"><i class="fa-brands fa-linkedin"></i> LinkedIn</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/Components/pages/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header';
+
+const Resume = () => {
+    return (
+        <div>
+            <h2>Resume</h2>
+            <img src={require('../../images/homegrown-sc.jpg').default} alt="My resume"></img>
+        </div>
+    )
+}
+
+export default Resume;
